ac98e34c7ba38cbbb27bf3ca2a813e2a7b28bf1c
Emi Fernandez <124414783+emilce-code@users.noreply.github.com>
2026-09-15 19:39:12 -0300

    Fix JwtAuthGuard test construction

--- apps/api/src/auth/jwt-auth.guard.spec.ts
+++ apps/api/src/auth/jwt-auth.guard.spec.ts
@@ -1,7 +1,12 @@
+import { PrismaService } from '../prisma/prisma.service';
+import { Auth0AuthGuard } from './auth0-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
 describe('JwtAuthGuard', () => {
   it('should be defined', () => {
-    expect(new JwtAuthGuard()).toBeDefined();
+    const auth0AuthGuard = {} as Auth0AuthGuard;
+    const prisma = {} as PrismaService;
+
+    expect(new JwtAuthGuard(auth0AuthGuard, prisma)).toBeDefined();
   });
 });
